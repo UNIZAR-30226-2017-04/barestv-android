574a7b5ee08f1c272feddc1623d34b8e5c79a6c6
crzyivo <ivan.nadie@gmail.com>
2017-03-23 12:32:48 +0100

    Cambiado activity_tabs, para que muestre fragmentos creados en otras clases. Creada una clase de ejemplo para fragmentos

--- app/src/main/java/unizar/margarethamilton/barestv_android/PlantillaFragment.java
+++ app/src/main/java/unizar/margarethamilton/barestv_android/PlantillaFragment.java
@@ -7,22 +7,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-/**
- * Created by ivo on 21/03/17.
- */
 
-public class TabBusquedaFragment extends Fragment {
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PlantillaFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link PlantillaFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PlantillaFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    public TabBusquedaFragment(){}
+    public PlantillaFragment() {
+        // Required empty public constructor
+    }
 
     /**
      * Use this factory method to create a new instance of
@@ -33,8 +42,8 @@ public class TabBusquedaFragment extends Fragment {
      * @return A new instance of fragment PlantillaFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TabBusquedaFragment newInstance(String param1, String param2) {
-        TabBusquedaFragment fragment = new TabBusquedaFragment();
+    public static PlantillaFragment newInstance(String param1, String param2) {
+        PlantillaFragment fragment = new PlantillaFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -50,9 +59,12 @@ public class TabBusquedaFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.busqueda_fragment_layout, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plantilla, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/unizar/margarethamilton/barestv_android/activity_tabs.java
+++ app/src/main/java/unizar/margarethamilton/barestv_android/activity_tabs.java
@@ -57,40 +57,7 @@ public class activity_tabs extends AppCompatActivity {
     }
 
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_activity_tabs, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -106,7 +73,9 @@ public class activity_tabs extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+
+            return  TabBusquedaFragment.newInstance("par1", String.valueOf(position));
+            //return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
